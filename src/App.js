c53a30cfbac2075351854da85902cab505309bf2
Anthon Fredriksson <hornta@protonmail.com>
2020-02-21 22:24:34 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Chart from "chart.js";
 
 function App() {
+  const canvasRef = React.useRef();
+
+  React.useEffect(() => {
+    const myChart = new Chart(canvasRef.current.getContext("2d"), {
+      type: "line",
+      data: [],
+      options: {
+        responsive: false
+      }
+    });
+  }, [canvasRef]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas ref={canvasRef} width="400" height="400" />
     </div>
   );
 }
